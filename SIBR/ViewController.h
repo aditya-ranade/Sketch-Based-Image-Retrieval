8397f9a8ffb94ab3170cfa537e39c9d3398472d9
Aditya Ranade <aranade@Adityas-MacBook-Pro-2.local>
2017-04-27 00:13:01 -0400

    Initial Commit

---
+++ SIBR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SIBR
+//
+//  Created by Aditya Ranade on 4/27/17.
+//  Copyright © 2017 Aditya Ranade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SIBR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SIBR
+//
+//  Created by Aditya Ranade on 4/27/17.
+//  Copyright © 2017 Aditya Ranade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
